ecada50799faf4b2bd3e91a4f2de6115d59a68a1
rnsloan <rnsloan1@gmail.com>
2018-01-30 15:22:11 +1100

    remove unneeded code

--- e2e/index.js
+++ e2e/index.js
@@ -1,22 +1,6 @@
 const puppeteer = require("puppeteer");
 const assert = require("assert");
 
-/*
-(async () => {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-  });
-  const page = await browser.newPage();
-  await page.goto("http://localhost:3000");
-  await page.screenshot({ path: "example.png" });
-
-  const title = await page.$('h1 a').textContent;
-  assert.equal(title, 'drinks');
-
-  await browser.close();
-})();
-*/
-
 (async () => {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
